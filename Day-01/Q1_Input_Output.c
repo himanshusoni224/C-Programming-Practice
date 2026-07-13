4923983a1c16af15fd87b40cb6ccb36771dad286
himanshusoni224 <himanshusoni200718@gmail.com>
2026-07-13 15:30:33 +0530

    Day 1: Q1 Input and Output using Variables

---
+++ Day-01/Q1_Input_Output.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+    int a , b;
+    
+    printf("enter first number :");
+    scanf("%d", &a);
+    
+    printf("enter second number :");
+    scanf("%d", &b);
+    
+    printf("first number : %d \n", a);
+    printf("second number : %d \n", b);
+    
+    
+    return 0;
+}    
+    
