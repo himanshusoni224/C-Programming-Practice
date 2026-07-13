a9d0226699250282098d1585d1a491626ddc98c9
himanshusoni224 <himanshusoni200718@gmail.com>
2026-07-13 16:02:36 +0530

    Day 01: Add Q3 Arithmetic Operations

---
+++ Day-01/Q3_Arithmetic_Operations.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int a, b;
+    
+    printf("enter first number : ");
+    scanf("%d", &a);
+    
+    printf("enter second number : ");
+    scanf("%d", &b);
+    
+    printf("a + b = %d \n", a + b);
+    printf("a - b = %d \n", a - b);
+    printf("a * b = %d \n", a * b);
+    printf("a / b = %d \n", a / b);
+    return 0;
+}    
+    
