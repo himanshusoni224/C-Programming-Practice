897c345e4372037053cc50ee7a170f94080734cb
himanshusoni224 <himanshusoni200718@gmail.com>
2026-07-15 19:50:12 +0530

    real number check program in C
    Added a C program to check if a number is natural.

---
+++ Day-01/Q5_Real_Number_Check.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int number;
+    printf("enter number : ");
+    scanf("%d", &number);
+
+    if(number >=1){
+        printf("natural number");
+    }
+    else {
+        printf("its not natural number");
+    } 
+ 
+return 0;
+}
